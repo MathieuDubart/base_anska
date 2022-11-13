5fe2c8ec49919a473fe5ead06d88d0a97e700e98
Mathieu <mathieu.dubart@icloud.com>
2022-11-13 13:56:02 +0100

    adding all days

--- src/script.js
+++ src/script.js
@@ -1,25 +1,17 @@
-let header = document.querySelector('header');
-let layer_one = document.getElementById('layer-one');
-let layer_two = document.getElementById('layer-two');
-let layer_three = document.getElementById('layer-three');
-let page_title = document.getElementById('page-title');
-let discover_btn = document.getElementById('discover-btn');
-let page_content = document.getElementById('content');
-let example_range = document.getElementById('exampleRange');
+let container = document.querySelector('.custom-container');
+let layers = document.querySelectorAll('.custom-layer');
+let innerWidth = window.innerWidth;
+let parallaxSpeed;
+container.onscroll = function (){
+    let X = container.scrollTop;
 
-window.addEventListener('scroll',e => {
-    let value = window.scrollY;
-    layer_three.style.top = value*0.25+'px';
-    layer_two.style.top = value*0.5+'px';
-    layer_one.style.top = value*0+'px';
-    page_title.style.marginLeft = value*3.5+'px';
-    page_title.style.marginTop = value*1.5+'px';
-    discover_btn.style.marginTop = value*1.5+'px';
-    header.style.top = value*0.5+'px';
-});
+    if (innerWidth > 2000) {
+        parallaxSpeed = 10;
+    } else {
+        parallaxSpeed = 16;
+    }
 
-let rangeValue;
-
-example_range.addEventListener('change', e=>{
-    rangeValue =  example_range.value;
-})
+    for(let i=1; i<layers.length+1;i++) {
+        layers[i-1].style.left=X/(parallaxSpeed/i)+'px';
+    }
+}
