9ea98b6ee7e97d7d0f0e3eb4f45a79f59a4a4c06
Mathieu <mathieu.dubart@icloud.com>
2022-11-20 16:11:57 +0100

    adding coords

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 let container = document.querySelector('.custom-container');
 let layers = document.querySelectorAll('.custom-layer');
 let innerWidth = window.innerWidth;
@@ -15,3 +17,41 @@ container.onscroll = function (){
         layers[i-1].style.left=X/(parallaxSpeed/i)+'px';
     }
 }
+
+// ----------------------- script for charts -----------------------------//
+
+google.charts.load("current", {packages:["corechart"]});
+google.charts.setOnLoadCallback(drawChart);
+function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+        ['', 'Gigatonnes de CO2'],
+        ['Contenues dans le Permafrost',     1600],
+        ['Relâchées en 2022', 32]
+    ]);
+
+    var options = {
+        title: 'Émissions de CO2 en Gigatonnes (Gt)',
+        pieHole: 0.4,
+        backgroundColor: 'transparent',
+
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+    chart.draw(data, options);
+}
+
+// ------------- script to play css when in viewport -------------- //
+const inViewport = (entries, observer) => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("is-inViewport", entry.isIntersecting);
+    });
+};
+
+const Obs = new IntersectionObserver(inViewport);
+const obsOptions = {}; //See: https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API#Intersection_observer_options
+
+// Attach observer to every [data-inviewport] element:
+const ELs_inViewport = document.querySelectorAll('[data-inviewport]');
+ELs_inViewport.forEach(EL => {
+    Obs.observe(EL, obsOptions);
+});
